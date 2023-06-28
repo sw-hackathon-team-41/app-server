e01370e26b9d17e3badb57c9da5b5167be541c9d
seungsu <h970126@gmail.com>
2023-06-28 21:27:32 +0900

    Initialize Project

---
+++ src/main/java/com/hackathon/herb/HerbApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.herb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbApplication.class, args);
+	}
+
+}
