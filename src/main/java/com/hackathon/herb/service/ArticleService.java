d214f64db37152efd848c92b58213f6592aab6a6
seungsu <h970126@gmail.com>
2023-06-30 00:35:36 +0900

    feat: api 고도화

--- src/main/java/com/hackathon/herb/service/ArticleService.java
+++ src/main/java/com/hackathon/herb/service/ArticleService.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
